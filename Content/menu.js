3832c2fd2173dde42f8a9f7e7f208edc691bf4a1
haze_y <linda.buile@gmail.com>
2019-03-11 12:32:45 +0200

    Adding Content files

---
+++ Content/menu.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $('.nav').on('click', '#hamburger', function () {
+        $('#menu').toggle().addClass('show-menu');
+    });
+});
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth > 480) {
+        $('#menu').removeClass('show-menu');
+    } else {
+        $('#menu').addClass('show-menu');
+    }
+});
+
+$('#userMenu').click(function () { 
+    $('.user-menu').toggleClass('show');
+});
